59f7ed58d7b86a6aa7ff0b70af6b93376d8cebf5
mhumpolec <martin.humpolec@gmail.com>
2016-06-29 21:10:59 +0000

    KBCParameters.java edited online with Bitbucket

--- main/keboola/salesforce/writer/config/KBCParameters.java
+++ main/keboola/salesforce/writer/config/KBCParameters.java
@@ -9,6 +9,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
