9d9d9e39fc9baff2838791220c79802b935e36a7
mhumpolec <martin.humpolec@gmail.com>
2016-06-29 21:38:16 +0000

    Writer.java edited online with Bitbucket

--- main/keboola/salesforce/writer/Writer.java
+++ main/keboola/salesforce/writer/Writer.java
@@ -77,12 +77,11 @@ public class Writer {
     	for (int i = 0; i < listOfFiles.length; i++) {
       	if (listOfFiles[i].isFile()) {
    		     System.out.println( "found file " + listOfFiles[i].getName());
-			JobInfo job = createJob(listOfFiles[i].getName().replace('.csv',''), connection);
+			JobInfo job = createJob(listOfFiles[i].getName().toString().replace('.csv',''), connection);
 			List<BatchInfo> batchInfoList = createBatchesFromCSVFile(connection, job, filesDirectory + listOfFiles[i].getName());
 			closeJob(connection, job.getId());
 			awaitCompletion(connection, job, batchInfoList);
 			checkResults(connection, job, batchInfoList);
-        	System.out.println("File " + );
       	  }
     	}
 		
