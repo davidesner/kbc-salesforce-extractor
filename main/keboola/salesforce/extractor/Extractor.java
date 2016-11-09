bd5f626434b8b320c3008fabc8751e3953cf2f5f
mhumpolec <martin.humpolec@gmail.com>
2016-11-09 15:40:05 +0100

    a

--- main/keboola/salesforce/extractor/Extractor.java
+++ main/keboola/salesforce/extractor/Extractor.java
@@ -60,7 +60,7 @@ public class Extractor {
     		List <String> objects = config.getParams().getObject();
     		List <String> soqls = config.getParams().getSOQL();
     		for( int i = 0; i < objects.size(); i++) {
-        		sfdown.runQuery( connection, outTablesPath, objects[i], soqls[i]);	
+        		sfdown.runQuery( connection, outTablesPath, objects.get(i-1), soqls.get(i-1));	
     		}
     	}
 				
