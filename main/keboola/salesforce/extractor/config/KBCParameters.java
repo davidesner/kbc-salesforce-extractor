fa145646e96bb3e5f0f5b376b47ac70e05384e04
mhumpolec <martin.humpolec@gmail.com>
2016-12-15 19:32:19 +0100

    x

--- main/keboola/salesforce/extractor/config/KBCParameters.java
+++ main/keboola/salesforce/extractor/config/KBCParameters.java
@@ -26,26 +26,7 @@ import java.util.logging.Logger;
  * @created 2015
  */
 
-public class objectsClass {
-	private List<String> objectname;
-	private List<String> objectsoql;
-	
-    public List<String> getObjectname() {
-        return objectname;
-    }
 
-    public void setObjectname(List<String> objectname) {
-        this.objectname = objectname;
-    }
-    public List<String> getObjectsoql() {
-        return objectsoql;
-    }
-
-    public void setObjectsoql(List<String> objectsoql) {
-        this.objectsoql = objectsoql;
-    }
-	
-}
 public class KBCParameters {
 
     private final static String[] REQUIRED_FIELDS = {"loginname", "password", "securitytoken", "object", "soql"};
---
+++ main/keboola/salesforce/extractor/config/objectsClass.java
@@ -0,0 +1,20 @@
+public class objectsClass {
+	private List<String> objectname;
+	private List<String> objectsoql;
+	
+    public List<String> getObjectname() {
+        return objectname;
+    }
+
+    public void setObjectname(List<String> objectname) {
+        this.objectname = objectname;
+    }
+    public List<String> getObjectsoql() {
+        return objectsoql;
+    }
+
+    public void setObjectsoql(List<String> objectsoql) {
+        this.objectsoql = objectsoql;
+    }
+	
+}
